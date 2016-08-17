79290c2b606cd4c45ef09855d78857fcca07407e
oh.kys <oh.kys@aiming-inc.com>
2016-08-17 18:22:45 +0900

    zsh.

--- setup.sh
+++ setup.sh
@@ -2,11 +2,14 @@ sudo yum -y install vim
 sudo yum -y install gcc-c++
 sudo yum -y install gdb
 sudo yum -y install clang
+sudo yum -y install zsh
 sudo yum -y install tmux
 sudo yum -y install lynx
 
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
-ln -sf ~/dotfile/bashrc ~/.bashrc
+ln -sf ~/dotfile/zshrc ~/.zshrc
 ln -sf ~/dotfile/vimrc ~/.vimrc
+
+chsh -s /bin/zsh
